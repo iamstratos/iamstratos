44aece9bb030f47e7c030fa71ada89b9a7332df2
ssstratos <ssstratos@gmail.com>
2017-06-10 01:27:34 +0300

    Changed to Reacti Starter Kit

--- 3daysberlin/src/main.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    var c = document.getElementByClassName("background");
-    var ctx = c.getContext("2d");
-    var img = document.getElementByClassName("bg-video");
-    ctx.drawImage(img,10,10);
-};
