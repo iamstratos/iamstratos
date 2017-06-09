2ed06e9e6fa0e43439e8cce0c77d6866d0e956c2
ssstratos <ssstratos@gmail.com>
2017-06-09 18:05:21 +0300

    Added timelapse video

--- 3daysberlin/src/app.jsx
+++ 3daysberlin/src/app.jsx
@@ -4,7 +4,8 @@ import '../styles/index.sass';
 export default class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="background js-background">
+        
         <h1>It Works!</h1>
         <p>This React project just works including <span className="redBg">module</span> local styles.</p>
         <p>Enjoy!</p>
