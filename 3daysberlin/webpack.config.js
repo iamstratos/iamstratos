fdef0b5a15851e39a1180db52d2fcff371b328c8
ssstratos <ssstratos@gmail.com>
2017-06-09 17:47:52 +0300

    Changed webpack build

--- 3daysberlin/webpack.config.js
+++ 3daysberlin/webpack.config.js
@@ -1,48 +1,65 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const path = require('path');
+"use strict";
+var webpack = require('webpack');
+var path = require('path');
+var loaders = require('./webpack.loaders');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var DashboardPlugin = require('webpack-dashboard/plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const HOST = process.env.HOST || "127.0.0.1";
+const PORT = process.env.PORT || "8888";
+
+loaders.push({
+  test: /\.sass$/,
+  loaders: ['style-loader', 'css-loader?importLoaders=1', 'sass-loader'],
+  exclude: ['node_modules']
+});
 
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.sass$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'sass-loader']
-                })
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        stats: 'errors-only',
-        open: true
-    },
-    plugins: [
-        new ExtractTextPlugin({
-            filename: 'app.css',
-            disable: false,
-            allChunks: true
-        }),
-        new HtmlWebpackPlugin({
-            title: '3 days in Berlin',
-            minify: {
-                collapseWhitespace: true
-            },
-            hash: true,
-            template: './src/index.ejs', // Load a custom template (ejs by default see the FAQ for details)
-        })
-   ]
-}
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.jsx', // your app's entry point
+  ],
+  devtool: process.env.WEBPACK_DEVTOOL || 'eval-source-map',
+  output: {
+    publicPath: '/',
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    loaders
+  },
+  devServer: {
+    contentBase: "./public",
+    // do not print bundle build stats
+    noInfo: true,
+    // enable HMR
+    hot: true,
+    open: true,
+    // embed the webpack-dev-server runtime into the bundle
+    inline: true,
+    // serve index.html in place of 404 responses to allow HTML5 history
+    historyApiFallback: true,
+    port: PORT,
+    host: HOST
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin({
+      filename: 'style.css',
+      allChunks: true
+    }),
+    new DashboardPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      files: {
+        css: ['style.css'],
+        js: [ "bundle.js"],
+      }
+    }),
+  ]
+};
