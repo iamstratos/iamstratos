57fef55a7e8f54b9c3ce1705259821cfafd3fe42
ssstratos <ssstratos@gmail.com>
2017-11-20 00:57:54 +0200

    Add Services-Inner to Satori

---
+++ satori/js/animations.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var controller = new ScrollMagic.Controller({
+  addIndicators: false
+});
+
+// module-photo scenes
+
+var $modulePhoto = $('.module-photo');
+
+$modulePhoto.each(function (_index, _elem) {
+
+  var tweenMotion = TweenMax.from($(this).find('img'), 1, { y: '-40%', ease: Power0.easeNone });
+  var tweenScale = TweenMax.from($(this).find('img'), 1, { scale: 1.1, ease: Power0.Linear });
+
+  var photoMotionScene = new ScrollMagic.Scene({
+    duration: '90%',
+    triggerElement: this,
+    triggerHook: 'onEnter',
+    reverse: true
+  }).setTween(tweenMotion).addTo(controller);
+
+  var photoScaleScene = new ScrollMagic.Scene({
+    duration: '40%',
+    triggerElement: this,
+    triggerHook: 'onEnter',
+    reverse: true
+  }).setTween(tweenScale).addTo(controller);
+});
