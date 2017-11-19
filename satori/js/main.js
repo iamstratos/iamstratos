2dc10c7b3c2e0cbad77728891058a87ccff6c45a
ssstratos <ssstratos@gmail.com>
2017-11-19 17:53:52 +0200

    Add Services page on Satori

--- satori/js/main.js
+++ satori/js/main.js
@@ -1,10 +1,24 @@
 'use strict';
 
+function setSideNav() {
+  var $sideNav = $('.side-nav');
+
+  $('[data-id]').each(function (_index, _elem) {
+
+    var thisTitle = $(_elem).attr('data-title');
+    var thisId = $(_elem).attr('id');
+
+    var sideContent = '<a class="side-content" href="#' + thisId + '">\n    <div class="side-num">' + (_index.toString().length <= 1 ? '0' + (_index + 1) : _index + 1) + '</div>\n    <div class="sm-bold">' + thisTitle + '</div>\n    <div class="side-line">\n      <div class="side-line-bg"></div>\n    </div>\n    <div class="side-rect"></div></a>';
+
+    $sideNav.find('.side-inner').append(sideContent);
+  });
+}
+
 $(document).ready(function () {
 
-  if ($('html').hasClass('trident') === false && $('html').hasClass('mobile') === false) {
-    $("body").niceScroll();
-  }
+  if ($('html').hasClass('trident') === false && $('html').hasClass('mobile') === false) $("body").niceScroll();
+
+  if ($('.side-nav').length > 0) setSideNav();
 
   $(document).on('mousemove', function (e) {
 
@@ -37,10 +51,29 @@ $(document).ready(function () {
     $("body").getNiceScroll().resize();
   });
 
-  $(document).on('click', '.side-nav .side-content', function () {
+  $(document).on('click', '.side-nav .side-content', function (e) {
+    e.preventDefault();
+
     if ($(this).hasClass('active') === false) {
-      $(this).siblings().removeClass('active');
-      $(this).addClass('active');
+      $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+      }, 1500);
     }
+  });
+
+  $(window).scroll(function () {
+    var scrollTop = $(document).scrollTop();
+    var anchors = $('body').find('.anchor');
+    var halfHeight = window.innerHeight / 2;
+    $('.side-nav').removeClass('active');
+
+    anchors.each(function (_index, _elem) {
+      if (scrollTop > $(_elem).offset().top - halfHeight && scrollTop < $(_elem).offset().top + $(_elem).height() - halfHeight) {
+        $('.side-nav .side-content[href="#' + $(_elem).attr('id') + '"]').addClass('active');
+        $('.side-nav').addClass('active');
+      } else {
+        $('.side-nav .side-content[href="#' + $(_elem).attr('id') + '"]').removeClass('active');
+      }
+    });
   });
 });
